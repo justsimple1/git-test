f3268a5dd3bbb58705be5a9c2d23614bcb9c0c61
justsimple1 <dla-raboti5@yandex.ru>
2018-03-24 23:54:59 +0300

    1.3. JavDoc. Code convetion

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 	package ru.job4j;
-public class Calculate{
 	
+	
+		/**
+	* Class для создания калькулятора.
+	* @author panin.nikita.
+	* @since 24.03.2018
+	* @version 1.
+	*/
+public class Calculate{
+	/**
+	* Main.
+	* @param args - args.
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello world");
 	}
