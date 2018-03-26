e739895514320577deef613d83e6ea222dc58535
justsimple1 <dla-raboti5@yandex.ru>
2018-03-26 22:45:03 +0300

    3.1. Элементарный калькулятор. CalculateTest.java

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -6,21 +6,49 @@ import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 /**
-* Test
-* @author panin.nikita(dla-raboti5@yandex.ru)	
-* @version 1
-* @since 25.03.2018
-*/
+ * Test
+ * @author panin.nikita(dla-raboti5@yandex.ru)
+ * @version 1
+ * @since 25.03.2018
+ */
 
 public class CalculateTest {
-	public void echoMethodVerification() {
-	String input = "All right!";
-	String expect = "Echo, echo, echo : All right!";
-	
-	Calculate calc = new Calculate();
-	
-	String result = calc.echo(input);	
-	
-	assertThat(result, is(expect));
+
+	@Test
+	public void whenAdd1Plus1Then2() {
+		Calculate calc = new Calculate();
+		calc.add(1D,1D);
+		double result = calc.getResult();
+		double expected = 2D;
+		assertThat(result, is(expected));
 	}
+
+	@Test
+	public void whenSub7On2Then5() {
+		Calculate calc = new Calculate();
+		calc.sub(7D,2D);
+		double result = calc.getResult();
+		double expected = 5D;
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void whenMul2On2Then4() {
+		Calculate calc = new Calculate();
+		calc.mul(2D,2D);
+		double result = calc.getResult();
+		double expected = 4D;
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void whenDiv8On2Then4() {
+		Calculate calc = new Calculate();
+		calc.div(8D,2D);
+		double result = calc.getResult();
+		double expected = 4D;
+		assertThat(result, is(expected));
+	}
+
+
 }
