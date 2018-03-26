fd8d6d1326c42b928cde4ce5a1dcaa73688f9f80
justsimple1 <dla-raboti5@yandex.ru>
2018-03-26 22:43:19 +0300

    3.1. Элементарный калькулятор. Calculate.java

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,26 +1,71 @@
-	package ru.job4j;
-	
-	
-	/**
-	* Class для создания калькулятора.
-	*
-	* @author panin.nikita.
-	* @since 24.03.2018.
-	* @version 1.
-	*/
+package ru.job4j;
+
+
+/**
+ * Class для создания калькулятора.
+ *
+ * @author panin.nikita.
+ * @since 24.03.2018.
+ * @version 1.
+ */
 public class Calculate {
 	/**
-	* Main.
-	* @param args args.
-	*/ 
+	 * result -Результат арифметических операций.
+	 */
+	private double result;
+
+	/**
+	 * Сложение
+	 * @param first - первое значение.
+	 * @param second - второе значение.
+	 * this.result - результат.
+	 */
+	public void add(double first,double second){
+		this.result = first + second;
+	}
+	/**
+	 * Вычитание
+	 * @param first - первое значение.
+	 * @param second - второе значение.
+	 * this.result - результат.
+	 */
+	public void sub(double first,double second){
+		this.result = first - second;
+	}
+	/**
+	 * Умножение
+	 * @param first - первое значение.
+	 * @param second - второе значение.
+	 * this.result - результат.
+	 */
+	public void mul(double first,double second){
+		this.result = first * second;
+	}
+	/**
+	 * Деление
+	 * @param first - первое значение.
+	 * @param second - второе значение.
+	 * this.result - результат.
+	 */
+	public void div(double first,double second){
+		this.result = first / second;
+	}
+
+	public double getResult(){
+		return this.result;
+	}
+	/**
+	 * Main.
+	 * @param args args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
 	/**
-	*Method echo.
-	*@param name yeah.
-	*return Echo yeah.
-	*/
+	 *Method echo.
+	 *@param name yeah.
+	 *return Echo yeah.
+	 */
 	public String echo(String name) {
 		return  "Echo, echo, echo : " + name;
 	}
