31b65e38f59ee31383cd8523563178623999f9f9
justsimple1 <dla-raboti5@yandex.ru>
2018-03-26 23:12:30 +0300

    3.1. Элементарный калькулятор. Upgrade Calculate.java

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -5,7 +5,7 @@ package ru.job4j;
  * Class для создания калькулятора.
  *
  * @author panin.nikita.
- * @since 24.03.2018.
+ * @since 26.03.2018.
  * @version 1.
  */
 public class Calculate {
@@ -58,15 +58,5 @@ public class Calculate {
 	 * Main.
 	 * @param args args.
 	 */
-	public static void main(String[] args) {
-		System.out.println("Hello world");
-	}
-	/**
-	 *Method echo.
-	 *@param name yeah.
-	 *return Echo yeah.
-	 */
-	public String echo(String name) {
-		return  "Echo, echo, echo : " + name;
-	}
+
 }
