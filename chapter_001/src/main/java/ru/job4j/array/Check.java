d24bdd26b4ad31f0da8a13f6e3269506fdf3ec93
justsimple1 <dla-raboti5@yandex.ru>
2018-06-08 13:04:39 +0300

    6.3. update Check.java

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -1,13 +1,13 @@
 package ru.job4j.array;
 
 public class Check {
-    public boolean mono(boolean data[]) {
-        boolean result = false;
+    public boolean mono(boolean[] data) {
+        boolean result = true;
         for (int i = 0; i < data.length; i++) {
-            if(data[data.length-1]!= data[i]){
+            if (data[data.length - 1] != data[i]) {
                 result = false;
                 break;
-            }result = true;
-        }return result;
+            }
+        } return result;
     }
 }
